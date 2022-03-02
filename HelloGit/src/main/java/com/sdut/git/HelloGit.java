40fd4c5c6e7de26a8543febf89557aa5922587bc
FlyingPikachuu <44453026+FlyingPikachuu@users.noreply.github.com>
2022-03-02 10:29:31 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/sdut/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.sdut.git;
+
+/**
+ * @author shkstart
+ * @create 2022/3/2 - 10:13
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
